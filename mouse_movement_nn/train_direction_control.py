1bb3f5be29e6a760876449cd9565995815913f10
Joe1234321Joe <drmanjo2020@gmail.com>
2017-07-24 22:29:03 -0700

    added_backup_capability

--- mouse_movement_nn/train_direction_control.py
+++ mouse_movement_nn/train_direction_control.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 from pandas import *
 import random
+import os.path
 
 from keras.models import Model
 from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input
@@ -24,10 +25,14 @@ from sklearn.utils import shuffle
 
 
 width_height = 224
-BATCH_SIZE = 144
+BATCH_SIZE = 259
 np.random.seed(42)
 
 def load_image(filename1, filename2, filename3, filename4):
+    assert os.path.isfile(filename1) 
+    assert os.path.isfile(filename2) 
+    assert os.path.isfile(filename3) 
+    assert os.path.isfile(filename4) 
     img1 = cv2.imread('{}'.format(filename1))
     img2 = cv2.imread('{}'.format(filename2))
     img3 = cv2.imread('{}'.format(filename3))
@@ -66,29 +71,31 @@ def get_x_data(images_df, positions_df, num_rows, directory):
         scaled = numpy_minmax(np.asarray([positions_df[i][0],positions_df[i][1],
                               positions_df[i][2],positions_df[i][3]]))
         dict_images = {'filename1': directory + '/' + images_df[i][0], 'filename2': directory + '/' + images_df[i][1],
-                       'filename3': directory + '/' + images_df[i+1][0], 'filename4': directory + '/' + images_df[i+1][1],
+                       'filename3': directory + '/' + images_df[i+1][0], 'filename4': directory + '/' + images_df[i][1],
                        'position.x': scaled[0],
                        'position.y': scaled[1],
                        'position.z': scaled[2],
                        'rotation.y': scaled[3],
                        'pathmarker1Points': positions_df[i][4],
                        'buoy1Points': positions_df[i][5],
-                       'buoy2Points': positions_df[i][6],
-                       'buoy3Points': positions_df[i][7],
-                       'pathmarker2Points': positions_df[i][8],
-                       'maneuverPoints': positions_df[i][9],
-                       'pathmarker3Points': positions_df[i][10],
-                       'pathmarker4Points': positions_df[i][11],
-                       'pathmarker5Points': positions_df[i][12],
-                       'octagon1Points': positions_df[i][13],
-                       'pathmarker6Points': positions_df[i][14],
-                       'pathmarker7Points': positions_df[i][15],
-                       'octagon2Points': positions_df[i][16]
+                       'buoy1Backup': positions_df[i][6],
+                       'buoy2Points': positions_df[i][7],
+                       'buoy2Backup': positions_df[i][8],
+                       'buoy3Points': positions_df[i][9],
+                       'pathmarker2Points': positions_df[i][10],
+                       'maneuverPoints': positions_df[i][11],
+                       'pathmarker3Points': positions_df[i][12],
+                       'pathmarker4Points': positions_df[i][13],
+                       'pathmarker5Points': positions_df[i][14],
+                       'octagon1Points': positions_df[i][15],
+                       'pathmarker6Points': positions_df[i][16],
+                       'pathmarker7Points': positions_df[i][17],
+                       'octagon2Points': positions_df[i][18]
                       }
         x_data.append(dict_images)
     df_x_data = pd.DataFrame(x_data, columns=['filename1', 'filename2', 'filename3', 'filename4',
                                                 'position.x', 'position.y', 'position.z', 'rotation.y',
-                                              'pathmarker1Points', 'buoy1Points', 'buoy2Points', 'buoy3Points',
+                                              'pathmarker1Points', 'buoy1Points','buoy1Backup', 'buoy2Points', 							'buoy2Backup','buoy3Points',
                                               'pathmarker2Points', 'maneuverPoints', 'pathmarker3Points',
                                              'pathmarker4Points', 'pathmarker5Points', 'octagon1Points',
                                               'pathmarker6Points','pathmarker7Points','octagon2Points'])
@@ -109,10 +116,10 @@ def get_y_data(positions_df, num_rows):
     y_data = []
     for i in range(0, num_rows-1):
         dict_images = {'mousex': positions_df[i+1][0], 'mousey': positions_df[i+1][1],
-                       'W': convert_y_wasd(positions_df[i+1][0]),
-                       'A': convert_y_wasd(positions_df[i+1][1]),
-                       'S': convert_y_wasd(positions_df[i+1][2]),
-                       'D': convert_y_wasd(positions_df[i+1][3])}
+                       'W': convert_y_wasd(positions_df[i+1][2]),
+                       'A': convert_y_wasd(positions_df[i+1][3]),
+                       'S': convert_y_wasd(positions_df[i+1][4]),
+                       'D': convert_y_wasd(positions_df[i+1][5])}
         y_data.append(dict_images)
     df_y_data = pd.DataFrame(y_data, columns=['mousex','mousey','W','A','S','D'])
     return df_y_data
@@ -130,7 +137,7 @@ def get_x_data_frame(data_frame, directory):
     images_df = data_frame[['filename1','filename2']].values
     non_image_df = data_frame[['position.x','position.y','position.z',
                                'rotation.y','pathmarker1Points', 'buoy1Points',
-                               'buoy2Points', 'buoy3Points',
+                               'buoy1Backup', 'buoy2Points','buoy2Backup',  'buoy3Points',
                                'pathmarker2Points', 'maneuverPoints',
                                'pathmarker3Points', 'pathmarker4Points',
                                'pathmarker5Points', 'octagon1Points',
@@ -139,7 +146,8 @@ def get_x_data_frame(data_frame, directory):
     return get_x_data(images_df, non_image_df, num_rows, directory)
 
 def get_training_df(bat_num, csv_file_name):
-    dir_path = '/home/joe/Dev/robot_direction_control/train_bat_'
+    dir_path = '/home/ubuntu/Dev/robot_direction_control/train_bat_'
+    #dir_path = '/train_bat_'
     directory = '{0}{1}'.format(dir_path, str(bat_num))
     csv_file = '{0}{1}/{2}'.format(dir_path, str(bat_num),csv_file_name)
     return [pd.read_csv(csv_file), directory]
@@ -163,7 +171,7 @@ td17 = get_training_df(17, 'labels.csv')
 td18 = get_training_df(18, 'labels.csv')
 td19 = get_training_df(19, 'labels.csv')
 td20 = get_training_df(20, 'labels.csv')
-
+td21 = get_training_df(21, 'labels.csv')
 
 X = get_x_data_frame(td1[0], td1[1])
 X = X.append(get_x_data_frame(td2[0], td2[1]), ignore_index=True)
@@ -185,6 +193,7 @@ X = X.append(get_x_data_frame(td17[0], td17[1]), ignore_index=True)
 X = X.append(get_x_data_frame(td18[0], td18[1]), ignore_index=True)
 X = X.append(get_x_data_frame(td19[0], td19[1]), ignore_index=True)
 X = X.append(get_x_data_frame(td20[0], td20[1]), ignore_index=True)
+X = X.append(get_x_data_frame(td21[0], td21[1]), ignore_index=True)
 y = get_y_data_frame(td1[0])
 y = y.append(get_y_data_frame(td2[0]), ignore_index=True)
 y = y.append(get_y_data_frame(td3[0]), ignore_index=True)
@@ -205,6 +214,11 @@ y = y.append(get_y_data_frame(td17[0]), ignore_index=True)
 y = y.append(get_y_data_frame(td18[0]), ignore_index=True)
 y = y.append(get_y_data_frame(td19[0]), ignore_index=True)
 y = y.append(get_y_data_frame(td20[0]), ignore_index=True)
+y = y.append(get_y_data_frame(td21[0]), ignore_index=True)
+
+
+#X_val = get_x_data_frame(val[0], val[1])
+#y_val = get_y_data_frame(val[0])
 
 def get_class_weights():
     data = get_y_data_frame(td1[0])
@@ -220,10 +234,14 @@ print(y)
 
 X, y = shuffle(X.values, y.values)
 
+### what nick changed ###
+#X_train, y_train = X, y#, y_train, y_valid = train_test_split(X, y, test_size=0.1)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.1)
+### what nick changed ###
 
+#X_valid, y_valid = X_val.values, y_val.values
 images = Input(shape=(224,224,3),name = 'image_input')
-non_image_df = Input(shape=(14,),name = 'non_image_input')
+non_image_df = Input(shape=(19,),name = 'non_image_input')
 inception_v3_model = InceptionV3(weights='imagenet', include_top=False)
 for layer in inception_v3_model.layers:
     layer.trainable=False
@@ -243,7 +261,7 @@ final = Dense(6, activation='linear')(final)
 #Create model 
 model = Model(input=[images, non_image_df], output=final)
 
-model.summary()
+model.summary()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 adam = Adam(lr=0.001, clipvalue=1.5)
 model.compile(loss='mse', optimizer=adam)
 
@@ -256,10 +274,10 @@ def generator(X, y, batch_size=BATCH_SIZE):
                 for x, y in zip(X_copy[i:i+batch_size], y_copy[i:i+batch_size]):
                     #print('x[0], x[1], x[2], x[3]', x[0], x[1], x[2], x[3])
                     try:
-                        rx1, rx2, ry = [normalize(load_image(x[0], x[1], x[2], x[3]))], [x[4] + random.gauss(0, .08)
-                                                                                         ,x[5] + random.gauss(0, .08)
-                                                                                         ,x[6] + random.gauss(0, .08)
-                                                                                         ,x[7] + random.gauss(0, .08)
+                        rx1, rx2, ry = [normalize(load_image(x[0], x[1], x[2], x[3]))],[.5#x[4] + random.gauss(0, .08)
+                                                                                         ,.5#x[5] + random.gauss(0, .08)
+                                                                                         ,.5#x[6] + random.gauss(0, .08)
+                                                                                         ,.5#x[7] + random.gauss(0, .08)
                                                                                          ,x[8]
                                                                                          ,x[9]
                                                                                          ,x[10]
@@ -270,6 +288,11 @@ def generator(X, y, batch_size=BATCH_SIZE):
                                                                                          ,x[15]
                                                                                          ,x[16]
                                                                                          ,x[17]
+											 ,x[18]
+											 ,x[19]
+											 ,x[20]
+											 ,x[21]
+											 ,x[22]
                                                                                             ], [y]
                         #print(x[0], x[1], x[2], x[3])
                         y = np.array(ry)
@@ -281,11 +304,11 @@ def generator(X, y, batch_size=BATCH_SIZE):
                 X_result_1, X_result_2, y_result = np.concatenate(X_image), np.concatenate(X_positions), np.concatenate(y_result)
                 yield [X_result_1, X_result_2], y_result
 
-EPOCHS = 60
-PER_EPOCH = 115
+EPOCHS = 500
+PER_EPOCH = 72
 
 class_w = get_class_weights()
-filepath="/home/joe/Dev/robot_direction_control/models/InceptionV3_fc512_X2_pos-{epoch:02d}.hdf5"
+filepath="/home/ubuntu/Dev/robot_direction_control/models/InceptionV3_fc512_X2_pos.hdf5"
 checkpoint = ModelCheckpoint(filepath, verbose=1, save_best_only=True)
 callbacks_list = [checkpoint]
 
@@ -296,6 +319,6 @@ history = model.fit_generator(
     validation_data=generator(X_valid, y_valid),
     #validation_steps=len(y_valid)//(BATCH_SIZE),
     class_weight = class_w,
-    validation_steps=2,
+    validation_steps=8,
     callbacks=callbacks_list
 )
